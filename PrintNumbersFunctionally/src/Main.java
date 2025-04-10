a6c18490fceb80d2c3281950f1acd59b8f24297b
Ashutosh Pathak <ashu.p89@gmail.com>
2025-04-10 11:32:47 +0530

    working like a charm

--- PrintNumbersFunctionally/src/Main.java
+++ PrintNumbersFunctionally/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Printing numbers functionally");
+        List<Integer> numbers = List.of(1,2,3,4,5,6,7,8);
+
+        printNumbersFunctionally(numbers);
+
+    }
+
+    private static void printAnInteger(int x){
+        System.out.println(x);
+    }
+
+    private static void printNumbersFunctionally(List<Integer> numbers) {
+        numbers.stream()
+                .forEach(Main::printAnInteger);
     }
 }

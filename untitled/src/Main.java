ee881f53cfc4bcbb09a8880b1fff8825e5b6016b
Ashutosh Pathak <ashu.p89@gmail.com>
2025-04-10 11:15:18 +0530

    my first functional program to just pring a list functionally using streams

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("Printing List of numbers functionally");
+
+        List<Integer> numbers = List.of(1,2,3,4,5,6,7,8);
+        printListFunctionally(numbers);
+
+
+
+
+    }
+
+    public static void printAnInteger(int x){
+        System.out.println(x);
+    }
+
+    public static void printListFunctionally( List<Integer> numbers ){
+        numbers.stream()
+                .forEach(Main::printAnInteger);
     }
 }
